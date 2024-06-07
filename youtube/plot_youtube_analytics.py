435bd751507b43a400f9c5540ddbd3e0d4f4fe7e
Edward Evans <elevans2@wisc.edu>
2024-06-06 21:39:54 -0500

    Perserve the 'Channel' column

--- youtube/plot_youtube_analytics.py
+++ youtube/plot_youtube_analytics.py
@@ -2,7 +2,6 @@ import argparse
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import code
 
 def load_csv(path: str) -> pd.DataFrame:
     """Load data from a CSV file into a pandas dataframe.
@@ -10,7 +9,7 @@ def load_csv(path: str) -> pd.DataFrame:
     return pd.read_csv(path, sep=",")
 
 
-def filter_dataframe(data: pd.DataFrame) -> pd.DataFrame:
+def filter_dataframe(data: pd.DataFrame, monthly: bool=False) -> pd.DataFrame:
     """Filter and re-format the YouTube pandas dataframe.
     """
     # remove zero view entries
@@ -18,20 +17,31 @@ def filter_dataframe(data: pd.DataFrame) -> pd.DataFrame:
 
     # squash date and view counts into one entry
     data.loc[:, 'Date'] = pd.to_datetime(data['Date'])
-    data = data.groupby('Date')['Views'].sum().reset_index()
+    data = data.groupby(['Date', 'Channel'])['Views'].sum().reset_index()
 
-    # resample data into month range
-    data.set_index('Date', inplace=True)
-    data_monthly = data.resample('ME').sum()
-    data_monthly.reset_index(inplace=True)
-
-    return data_monthly
+    # store channel name and reset 'Channel' column
+    ch = data['Channel'][0]
+    if monthly:
+        # resample the data to be monthly
+        data.set_index('Date', inplace=True)
+        data_monthly = data.resample('ME').sum()
+        data_monthly.reset_index(inplace=True)
+        data_monthly['Channel'] = ch
+        return data_monthly
+    else:
+        return data
 
 
 def lineplot(data: pd.DataFrame, x: str="Date", y: str="Views"):
     """Plot a lineplot of the given data.
     """
-    return sns.lineplot(data=data, x=x, y=y)
+    return sns.lineplot(data=data,
+                        x=x,
+                        y=y,
+                        markers=True,
+                        dashes=False,
+                        hue="Channel",
+                        style="Channel")
 
 
 # set up argparse
@@ -47,6 +57,3 @@ df = load_csv(pargs.input)
 df = filter_dataframe(df)
 g = lineplot(df)
 plt.show()
-
-# debugging
-code.interact(local=locals())
